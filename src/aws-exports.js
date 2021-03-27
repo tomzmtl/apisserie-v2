2c81296f3da73e49287d8c3fb0e41d384ac664dd
Thomas Andreo <thomas.andreo@gmail.com>
2021-03-27 12:44:38 -0400

    updated aws-exports

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -6,20 +6,11 @@ const awsmobile = {
     "aws_dynamodb_all_tables_region": "us-east-1",
     "aws_dynamodb_table_schemas": [
         {
-            "tableName": "products-staging",
-            "region": "us-east-1"
-        },
-        {
             "tableName": "productsTable-staging",
             "region": "us-east-1"
         }
     ],
     "aws_cloud_logic_custom": [
-        {
-            "name": "productsApi",
-            "endpoint": "https://s4tevtip5c.execute-api.us-east-1.amazonaws.com/staging",
-            "region": "us-east-1"
-        },
         {
             "name": "productsApi",
             "endpoint": "https://3spo6acgi3.execute-api.us-east-1.amazonaws.com/staging",
