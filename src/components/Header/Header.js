262df9dddebd5e98a3b8927eb589cd0dc42b47aa
Thomas Andreo <thomas.andreo@gmail.com>
2021-03-28 11:45:06 -0400

    fixed refresh logic

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,14 @@
-import API from '@aws-amplify/api';
 import { ButtonBase, LinearProgress } from '@material-ui/core'
 import { Edit, PlaylistAddCheck, ShoppingCart, Sync } from '@material-ui/icons';
-import { useDispatch } from 'react-redux';
-import { API_NAME } from '../../constants';
 import { useLoadProducts, useNavigation } from '../../hooks';
-import { setProducts } from '../../redux/actions/products';
 import "./styles.scss";
 
 const Header = () => {
-  const isLoading = useLoadProducts()
+  const { isLoading, sendRequest } = useLoadProducts()
   const navigateTo = useNavigation()
-  const dispatch = useDispatch()
 
   const onClickRefresh = () => {
-    API
-      .get(API_NAME, '/products/id')
-      .then(response => dispatch(setProducts(response)))
-      .catch(error => console.log(error.response));
+    sendRequest()
   }
 
   return (
--- src/hooks.js
+++ src/hooks.js
@@ -1,25 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { API } from 'aws-amplify';
 import { useDispatch } from 'react-redux';
 import { setProducts } from './redux/actions/products'
 import { useHistory } from 'react-router';
 import { API_NAME } from './constants';
 
+const makeTimestamp = () => Math.round((new Date()).getTime() / 1000)
+
 export const useLoadProducts = () => {
   const dispatch = useDispatch()
   const [isLoading, setIsLoading] = useState(false)
+  const [ts, setTs] = useState(makeTimestamp())
+
+  const sendRequest = useCallback(
+    () => {
+      setTs(makeTimestamp())
+
+      API
+        .get(API_NAME, '/products/id')
+        .then(response => dispatch(setProducts(response)))
+        .catch(error => console.log(error.response))
+        .finally(() => setIsLoading(false))
+    }, [dispatch])
   
   useEffect(() => {
     setIsLoading(true)
 
-    API
-      .get(API_NAME, '/products/id')
-      .then(response => dispatch(setProducts(response)))
-      .catch(error => console.log(error.response))
-      .finally(() => setIsLoading(false))
-  }, [dispatch])
+    sendRequest()
+  }, [dispatch, sendRequest, ts])
 
-  return isLoading
+  return { sendRequest, isLoading }
 }
 
 export const useNavigation = () => {
