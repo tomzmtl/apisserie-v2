fe5839b7ae61f06b178609282fd79bf9e193707b
Thomas Andreo <thomas.andreo@gmail.com>
2022-04-10 16:52:23 -0400

    Added zone reordering

--- src/components/Zones/Zones.js
+++ src/components/Zones/Zones.js
@@ -1,24 +1,75 @@
 import { useSelector } from "react-redux"
-import { Stack, Card, CardActionArea, CardHeader } from "@mui/material"
-import { Add } from "@mui/icons-material"
+import {
+  Stack,
+  Card,
+  CardActionArea,
+  CardHeader,
+  IconButton,
+} from "@mui/material"
+import { Add, ArrowCircleUp } from "@mui/icons-material"
 import { selectZonesByOrder } from "../../selectors/zones"
 import { useZoneEdit } from "../Zone/hooks"
+import * as api from "../../api/zones"
 import "./styles.scss"
+import { useState } from "react"
+import { updateZone } from "../../actions/zones"
+import { useDispatch } from "react-redux"
 
 const Zones = () => {
   const zones = useSelector(selectZonesByOrder)
   const { zoneEditComponents, openZoneEdit } = useZoneEdit()
+  const [loading, setLoading] = useState([])
+  const dispatch = useDispatch()
 
   const renderZones = () =>
-    zones.map((zone) => {
+    zones.map((zone, index) => {
       const handleCardClick = () => {
         openZoneEdit(zone.id)
       }
 
+      const isLoading = loading.includes(zone.id)
+
+      const handleMoveUp = (e) => {
+        e.stopPropagation()
+
+        const previousZone = zones[index - 1]
+
+        setLoading([previousZone.id, zone.id])
+
+        const currentUpdate = { ...zone, order: index - 1 }
+        const prevUpdate = { ...previousZone, order: previousZone.order + 1 }
+
+        Promise.all([api.putZone(currentUpdate), api.putZone(prevUpdate)])
+          .then(() => {
+            dispatch(updateZone(currentUpdate))
+            dispatch(updateZone(prevUpdate))
+          })
+          .finally(() => {
+            setLoading([])
+          })
+      }
+
+      const cardHeaderProps = {
+        action: (
+          <IconButton
+            aria-label="settings"
+            onClick={handleMoveUp}
+            disabled={index === 0}
+            component="div"
+          >
+            <ArrowCircleUp />
+          </IconButton>
+        ),
+        title: zone.name,
+      }
+
       return (
-        <Card key={zone.id}>
-          <CardActionArea onClick={handleCardClick}>
-            <CardHeader avatar={`${zone.order}.`} title={zone.name} />
+        <Card key={zone.id} sx={{ opacity: isLoading ? 0.5 : 1 }}>
+          <CardActionArea
+            onClick={handleCardClick}
+            disabled={loading.includes(zone.id)}
+          >
+            <CardHeader {...cardHeaderProps} />
           </CardActionArea>
         </Card>
       )
