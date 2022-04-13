54bf063b37f04beff18a2ce8066c3faf30aabd79
Thomas Andreo <thomas.andreo@gmail.com>
2022-04-12 22:13:52 -0400

    Added empty products list splash icon

--- src/components/ShoppingList/ShoppingList.js
+++ src/components/ShoppingList/ShoppingList.js
@@ -4,6 +4,7 @@ import { selectShoppingList } from "../../selectors/products"
 import { useProductEdit } from "../Product/hooks"
 import "./styles.scss"
 import ProductCard from "../ProductCard"
+import { PlaylistAddCheck } from "@mui/icons-material"
 
 const ShoppingList = () => {
   const { productEditComponents, openProductEdit } = useProductEdit()
@@ -23,8 +24,23 @@ const ShoppingList = () => {
       return <ProductCard {...cardProps} />
     })
 
-  const renderProductsByZone = (productsByZone) =>
-    productsByZone.map((zone) => (
+  const renderProductsByZone = (productsByZone) => {
+    if (!productsByZone.length) {
+      return (
+        <PlaylistAddCheck
+          color="disabled"
+          sx={{
+            display: "block",
+            margin: "auto",
+            mt: 12,
+            fontSize: 300,
+            opacity: 0.25,
+          }}
+        />
+      )
+    }
+
+    return productsByZone.map((zone) => (
       <div className="ShoppingList__zone" key={zone.id}>
         <Divider textAlign="left" sx={{ mb: 1 }}>
           {zone.name}
@@ -34,6 +50,7 @@ const ShoppingList = () => {
         </Stack>
       </div>
     ))
+  }
 
   return (
     <div className="List">
